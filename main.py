f4d0eddf527fa0189b917671eceb632fd044055c
trl242 <royaurallockhart@gmail.com>
2021-05-12 13:39:57 -0700

    setup test

--- main.py
+++ main.py
@@ -36,4 +36,49 @@ x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 #create neaural network
+model = Sequential()
 
+#LSTM longshort
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1, 1])))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+#testing model
+test_start = dt.datetime(2019,1,1)
+
+test_end = dt.datetime.now()
+
+test_data = web.DataReader(f'{financial_product}-{against_currency}', 'yahoo', test_start, test_end)
+actual_prices = test_data['Close'].values
+
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+
+model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+model_inputs = model_inputs.reshape(-1, 1)
+model_inputs = scaler.fit_transform(model_inputs)
+
+x_test = []
+
+for x in range(prediction_days, len(model_inputs)):
+    x_test.append(model_inputs[x-prediction_days:, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+prediction_prices = model.predict(x_test)
+prediction_prices = scaler.inverse_transform((prediction_prices))
+
+plt.plot(actual_prices, color='black', label='Actual Prices')
+plt.plot(prediction_prices, color='green', label='Predicted Prices')
+plt.title(f'{financial_product} price prediction')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend(loc='upper left')
+plt.show()

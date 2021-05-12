c31d51d79152212a81366f5bc3f3a4775225e274
trl242 <royaurallockhart@gmail.com>
2021-05-12 13:18:02 -0700

    Prepared data for machine learning

--- main.py
+++ main.py
@@ -8,13 +8,32 @@ from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 from tensorflow.keras.models import Sequential
 
+#Fiancial prodcut being tracked
 financial_product = 'BTC'
+#Currency the finacial product is to be tracked to
 against_currency = 'CAD'
 
+#date range for data
 start = dt.datetime(2016,1,1)
 end = dt.datetime.now()
 
 data = web.DataReader(f'{financial_product}-{against_currency}', 'yahoo', start, end)
 
 #prepare data for machine learning
-print(data.head())
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+
+#number of prediction days
+prediction_days = 60
+
+x_train, y_train = [], []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x, 0])
+    y_train.append(scaled_data[x, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+#create neaural network
+
